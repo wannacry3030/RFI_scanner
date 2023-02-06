6bf580750aa469531ee695f2cd1d42950ee68cb5
wannacry3030 <wannacry.bat@proton.me>
2023-02-06 09:48:23 -0300

    att

--- rfi_scanner.py
+++ rfi_scanner.py
@@ -7,24 +7,18 @@ root = tk.Tk()
 root.geometry("700x400")
 root.title("RFI scanner")
 
-#criando o input
-url_label = tk.label(root, text="URL")
-url_label.grid(row=0, column=0, sticky="W", padx=10, pady=10)
-
-#barra de progresso
-pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
-pb.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="e")
-
-#
-
 def run_scan():
     result_label.config(text="")
     pb.start(100)
     update_progress_bar()
     url = entry.get()
     
-    # list of files to check for RFI
     files = ["/etc/passwd", "/proc/self/environ", "../../../../../../../etc/passwd"]
+   
+#barra de progresso
+pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
+pb.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="e")
+
     
 for file in files:
     rfi_url = url + "?page" + file
@@ -33,4 +27,39 @@ for file in files:
         result_label.config(text=result_label.cget("text") + f"[+] is vulnerable to RFI: {rfi_url}")
     else:
         result_label.config(text=result_label.cget("text") + f"[+] is not vulnerable to RFI:{rfi_url}")
+pb.stop()
 
+def update_progress_bar():
+    pb["value"] = pb["value"] + 10
+    if pb["value"] <=100:
+        root.after(100, update_progress_bar)
+    else:
+        pb["value"] = 0
+
+
+
+
+
+#criando o input
+url_label = tk.Label(root, text="URL")
+url_label.grid(row=0, column=0, sticky="W", padx=10, pady=10)
+
+
+#input URL
+entry = tk.Entry(root)
+entry.grid(row=0, column=1, columnspan=2, padx=10,pady=10)
+
+#botao de scan
+scan_bt = tk.Button(root, text="Scanear", command=run_scan)
+scan_bt(row=2, column=0, padx=10, pady=10, sticky="e")
+
+#botao de reset
+reset_bt = tk.Button(root, text="Reset", command=reset_scan)
+reset_bt(row=2, column=1, padx=10, pady=10, sticky="w")
+
+#resultado
+result_label = tk.Label(root, text="")
+result_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+
+#startando a gui
+root.mainloop()
